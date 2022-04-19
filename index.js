e5fd6b483f7592b74cc588f680f0b7cea904ab72
James Coffman <jamesc4023@gmail.com>
2022-04-19 16:09:09 -0400

    commit tree 59848ea2ebcc4ac4c3b9d45218f033ea26536672

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+// function getReverse(word){
+
+//   let reverse = '';
+
+//   for(let i = word.length - 1; i >= 0; i--){
+//     reverse += word.charAt(i);
+//   }
+
+//   return reverse;
+
+// }
+
+// function isPalindrome(word) {
+
+//   if(word === word.split("").reverse().join("")){
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+  // let reverse = getReverse(word)
+
+  // if(word === reverse){
+  //   return true
+  // } else {
+  //   return false
+  // }
+
+// }
+
+function isPalindrome(word){
+  for(let i = 0; i < word.length / 2; i++){
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]){
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
@@ -8,6 +45,7 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  is Palindrome uses a function to reverse a word then checks if that word is equal to the word passed in as an argument
 */
 
 // You can run `node index.js` to view these console logs
